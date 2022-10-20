5f4597122ddff92b27330d5517c4176d4d4de0d3
Gustavo Colombo Rocha <gusttavocolombo@gmail.com>
2022-10-19 21:38:16 -0300

    feat: add protection to brute-force atack

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './modules/auth/auth.module';
 import { DressmakerModule } from './modules/dressmaker/dressmaker.module';
 import { UsersModule } from './modules/users/users.module';
 import { PrismaService } from './shared/infra/prisma/prisma.service';
 
 @Module({
-  imports: [UsersModule, AuthModule, DressmakerModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    UsersModule,
+    AuthModule,
+    DressmakerModule,
+  ],
   controllers: [],
-  providers: [PrismaService],
+  providers: [PrismaService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
--- src/modules/dressmaker/infra/http/express/controller/dressmaking.controller.ts
+++ src/modules/dressmaker/infra/http/express/controller/dressmaking.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiOkResponse,

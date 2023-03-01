44c6a1ab893e4fadd30ceedc69d13eed3b6ce49b
Gustavo Colombo Rocha <gusttavocolombo@gmail.com>
2023-03-01 14:18:56 -0300

    feat: adding documentation in endpoints of order

--- src/modules/order/controllers/order.controller.ts
+++ src/modules/order/controllers/order.controller.ts
@@ -1,4 +1,13 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { CreateOrderDTO } from '../dtos/CreateOrderDTO';
 import { CreateOrderService } from '../services/create-order-service';
 import { GetOneOrderService } from '../services/get-one-order-service';
@@ -12,6 +21,25 @@ export class OrderController {
     private getOrderByUserService: GetOrderByUserService,
   ) {}
 
+  @ApiResponse({ description: 'Request to create an order of service' })
+  @ApiBearerAuth()
+  @ApiCreatedResponse({
+    status: 201,
+    description: 'The order has been create successfully',
+  })
+  @ApiUnauthorizedResponse({
+    status: 404,
+    description: 'User not authorized, JWT token is missing',
+  })
+  @ApiNotFoundResponse({
+    status: 401,
+    description:
+      'Fail at create order service, user or dressmaking id is not valid',
+  })
+  @ApiInternalServerErrorResponse({
+    status: 500,
+    description: 'Internal server error, contact developer team',
+  })
   @Post()
   async createOrder(
     @Body() { user_id, dressmaking_id, tag, delivery_option }: CreateOrderDTO,
@@ -24,11 +52,49 @@ export class OrderController {
     });
   }
 
+  @ApiResponse({ description: 'Request to get specific order of service' })
+  @ApiBearerAuth()
+  @ApiOkResponse({
+    status: 200,
+    description: 'The order has been recovered successfully',
+  })
+  @ApiUnauthorizedResponse({
+    status: 404,
+    description: 'User not authorized, JWT token is missing',
+  })
+  @ApiNotFoundResponse({
+    status: 401,
+    description: 'Fail at get order service, order id is not valid',
+  })
+  @ApiInternalServerErrorResponse({
+    status: 500,
+    description: 'Internal server error, contact developer team',
+  })
   @Get('/:id')
   async getOneOrder(@Param('id') id: string) {
     return await this.getOneOrderService.execute(id);
   }
 
+  @ApiResponse({
+    description: 'Request to get order of service from specific user',
+  })
+  @ApiBearerAuth()
+  @ApiOkResponse({
+    status: 200,
+    description: 'The orders has been recovered successfully',
+  })
+  @ApiUnauthorizedResponse({
+    status: 404,
+    description: 'User not authorized, JWT token is missing',
+  })
+  @ApiNotFoundResponse({
+    status: 401,
+    description: 'Fail at get order service, user id is not valid',
+  })
+  @ApiInternalServerErrorResponse({
+    status: 500,
+    description: 'Internal server error, contact developer team',
+  })
   @Get('/user/:user_id')
   async getOrderByUser(@Param('user_id') user_id: string) {
     return await this.getOrderByUserService.execute(user_id);
